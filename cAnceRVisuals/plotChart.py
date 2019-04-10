0962659e58d1a0a2afd264221d40a1d64b025677
hannarii <hannarii@uoregon.edu>
2019-04-10 15:34:43 +0300

    editing plotChart.py

--- cAnceRVisuals/plotChart.py
+++ cAnceRVisuals/plotChart.py
@@ -1,12 +1,26 @@
-#!/usr/bin/env python
-# coding: utf-8
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 10 14:23:45 2019
+"""
 
+import csv
 import pandas as pd
 import numpy as np
 from bokeh.plotting import figure, output_file, show
 
-output_file("cAnceR_plotchart1.html")
-dt = pd.read_excel('/Users/ylhaart/TUNI.fi/Hanna-Riikka Rantamaa - Interaction techniques project/Data/Age_specific_incidence_by_cancer.xlsx', usecols='A:S',skiprows=0,nrows=1)
-dt
-dt['Cancer']
-dt['0-4']
+#open .csv file
+with open('plotData.csv') as csvfile:
+    readCSV = csv.reader(csvfile, delimiter=',')
+    line_count = 0
+    for row in readCSV:
+        #here we check only the line with the wanted cancer type
+        if (line_count == 4):
+            #here we create the plot chart with wanted cancer type and the right values.
+            print(row[2].split(";")[0])
+            line_count = line_count + 1
+        else:
+            line_count = line_count + 1
+
+     
+            #num = int(float(row[0].split(";")[]))
+          
