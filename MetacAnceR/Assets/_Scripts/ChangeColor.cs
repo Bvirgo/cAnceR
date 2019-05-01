7ccccead9cc9182e88e135b3085b3eb91a2c52d0
Arttu Ylhävuori <areee@users.noreply.github.com>
2019-05-01 12:53:01 +0300

    finished change color script + added the script to hand trigger

--- MetacAnceR/Assets/_Scripts/ChangeColor.cs
+++ MetacAnceR/Assets/_Scripts/ChangeColor.cs
@@ -1,15 +1,35 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class ChangeColor : MonoBehaviour {
+public class ChangeColor : MonoBehaviour
+{
+
+    private Renderer rend;
+    private Color colorAtStartUp;
+    private bool onOrOff = false;
 
     [SerializeField]
-    private Color colorTurnTo = Color.white;
+    private Color colorToTurnTo = Color.white;
 
-	public void ToggleColor()
+
+    public void ToggleColor()
     {
-        Renderer rend = gameObject.GetComponent<Renderer>();
-        Color currentColor = rend.material.color;
+
+        if (!onOrOff)
+        {
+            onOrOff = true;
+            rend.material.color = colorToTurnTo;
+        }
+        else
+        {
+            onOrOff = false;
+            rend.material.color = colorAtStartUp;
+        }
+
+    }
+    private void Start()
+    {
+        rend = gameObject.GetComponent<Renderer>();
+        colorAtStartUp = rend.material.color;
+        Debug.Log(colorAtStartUp);
     }
 }
