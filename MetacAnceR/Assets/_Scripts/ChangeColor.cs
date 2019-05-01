f492c64b7fd67f8aa68105b517a46296869f475a
Arttu Ylhävuori <areee@users.noreply.github.com>
2019-05-01 11:07:43 +0300

    started to code the color changer script for body parts

---
+++ MetacAnceR/Assets/_Scripts/ChangeColor.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeColor : MonoBehaviour {
+
+    [SerializeField]
+    private Color colorTurnTo = Color.white;
+
+	public void ToggleColor()
+    {
+        Renderer rend = gameObject.GetComponent<Renderer>();
+        Color currentColor = rend.material.color;
+    }
+}
