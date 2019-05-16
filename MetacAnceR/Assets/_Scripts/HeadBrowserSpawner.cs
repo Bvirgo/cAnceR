ee410953c648b92623adf4185f0c0ba0a5b83ca9
Arttu Ylhävuori <areee@users.noreply.github.com>
2019-05-16 16:17:46 +0300

    added new spawner for head browser

---
+++ MetacAnceR/Assets/_Scripts/HeadBrowserSpawner.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace ZenFulcrum.EmbeddedBrowser
+{
+    [RequireComponent(typeof(Browser))]
+    public class HeadBrowserSpawner : MonoBehaviour, INewWindowHandler
+    {
+        public Transform spawnPosition;
+        public float size;
+
+        public void Start()
+        {
+            GetComponent<Browser>().SetNewWindowHandler(Browser.NewWindowAction.NewBrowser, this);
+        }
+
+        public Browser CreateBrowser(Browser parent)
+        {
+            return null;
+        }
+
+    }
+}
