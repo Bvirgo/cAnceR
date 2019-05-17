e2efd4f278413104d0a5b62b222033f636e2f6f9
Arttu Ylhävuori <areee@users.noreply.github.com>
2019-05-17 10:17:25 +0300

    coded movements for visualization camera

--- MetacAnceR/Assets/_Scripts/HeadBrowserSpawner.cs
+++ MetacAnceR/Assets/_Scripts/HeadBrowserSpawner.cs
@@ -15,7 +15,16 @@ namespace ZenFulcrum.EmbeddedBrowser
 
         public Browser CreateBrowser(Browser parent)
         {
-            return null;
+            var ball = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+            ball.AddComponent<Rigidbody>();
+            ball.transform.localScale = new Vector3(size, size, size);
+            ball.transform.position = spawnPosition.position;
+
+            var browser = ball.AddComponent<Browser>();
+            browser.UIHandler = null;
+            browser.Resize(110, 110);
+
+            return browser;
         }
 
     }
