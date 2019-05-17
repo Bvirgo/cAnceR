f1c1a4a56800c3ef3d8926ed871a8f95b7de5f24
Arttu Ylhävuori <areee@users.noreply.github.com>
2019-05-17 15:08:42 +0300

    added toggle for canvas

---
+++ MetacAnceR/Assets/_Scripts/ToggleCanvas.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class ToggleCanvas : MonoBehaviour
+{
+
+    public void ToggleVisibility()
+    {
+        Canvas c = gameObject.GetComponent<Canvas>();
+        if (c.enabled)
+        {
+            c.enabled = false;
+        }
+        else
+        {
+            c.enabled = true;
+        }
+    }
+}
